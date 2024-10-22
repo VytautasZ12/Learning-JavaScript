2e53ef362d870b4cd621425d439a9d9165fcb329
Vytautas <vzvytautasz@gmail.com>
2024-10-22 13:21:13 +0300

    Aritchmetic expressions lesson.

---
+++ 2. Arithmetic expresions/index.js
@@ -0,0 +1,37 @@
+/*
+ arithmetic expression is a combination of...
+ operands (values, variables, etc.)
+ operators (+ - * / %)
+ that can be evaluated to a value
+ ex. y = x + 5;
+*/
+
+// let students = 20;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// let extraStudents = students % 3; using % operator good way to find that number is even or odd.
+
+// += augmented assignment operator. (Išplėstinio priskyrimo operatorius.)
+// good use for writing all code above.
+
+// students += 1;
+// students -= 1;
+// students *= 2;
+// students /= 2;
+
+// console.log(students);
+
+/*
+    Operator precedence. (Operatoriaus pirmenybė)
+    1. Parenthesis (). (Skliaustai)
+    2. Exponents. (Rodikliai)
+    3. Multiplication and division. (Daugyba ir dalyba)
+    4. Addition & substraction. (Sudėtis ir atimtis)
+*/
+
+// let result = 1 + 2 * (3 + 4);
+
+// console.log(result); solving evertyhig by priority left to right. First will be (), than * and than +.
