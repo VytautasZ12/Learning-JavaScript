538a3c6e8cfd586c45293c9bed974334964bd3c4
Vytautas <vzvytautasz@gmail.com>
2024-10-22 14:20:52 +0300

    Type conversion lesson.

---
+++ 4. Type conversion/index.js
@@ -0,0 +1,22 @@
+// Type Conversion = change the datatype of a value to another (strings, numbers, booleans).
+
+// let age = window.prompt("How old are you?");
+
+// console.log(typeof age);
+// age = Number(age); // this converting string to a number, becouse age is a string then converted to a number to check add tupeof.
+// console.log(typeof age);
+// age += 1;
+
+// console.log("Happy Birthday! You are", age + " year old");
+
+let x;
+let y;
+let z;
+
+x = Number("3.14");
+y = String(3.14);
+z = Boolean(""); // if write anything in "" will get true. Good use for user input.
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
