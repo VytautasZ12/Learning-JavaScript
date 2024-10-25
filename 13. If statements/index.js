0bb9c7a7594d287a7fc000b5b1f9ad30b43e7213
Vytautas <vzvytautasz@gmail.com>
2024-10-25 11:42:35 +0300

    If statement examples.

---
+++ 13. If statements/index.js
@@ -0,0 +1,32 @@
+// if statement = a basic form of decision making if a condition is true, then do somthing
+//                  if not, then dont do it!
+
+// order of if staments is matter, if wanna check more than 1 if statment its important to check them by writting in correct order.
+
+/*
+let age = 64;
+
+if (age >= 65) {
+  console.log("You are a senior citizen!");
+}
+else if (age >= 18) {
+  console.log("You are an adult!");
+} 
+else if (age < 0) {
+  console.log("You havent been born yet!");
+} 
+else {
+  console.log("You are a child!");
+}
+*/
+
+// this example for boolean value.
+
+let online = false;
+
+if (online){
+    console.log("You are online!")
+}
+else{
+    console.log("You are offline!")
+}
