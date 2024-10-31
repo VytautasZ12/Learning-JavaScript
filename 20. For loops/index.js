6ea39c6e1a8eab241da8c3624f80b509345f2946
Vytautas <vzvytautasz@gmail.com>
2024-10-31 12:12:02 +0200

    For loop.

---
+++ 20. For loops/index.js
@@ -0,0 +1,15 @@
+// for loop = repeat some code a certain amount of times.
+// first statment, second is condition and will continue statment as log as this condition is true
+// third stament incrimates first statment.
+
+for (let i = 1; i <= 10; i += 1) {
+  console.log(i);
+}
+console.log("Done");
+
+// count down.
+
+for (let i = 10; i > 0; i -= 1) {
+  console.log(i);
+}
+console.log("Done");
