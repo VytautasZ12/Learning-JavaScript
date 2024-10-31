91cd7692b57263a68cf255250b38a3a842d82030
Vytautas <vzvytautasz@gmail.com>
2024-10-31 11:44:49 +0200

    ! NOT logical operator.

---
+++ 17. NOT logical operator/index.js
@@ -0,0 +1,17 @@
+// ! NOT logical operator
+// typically used to reverse a condition's boolean value
+// true -> false   false -> true
+
+let temp = 15;
+let sunny = true;
+// ! if temp not greater than zero.
+if (!(temp > 0)) {
+  console.log("Its cold outside");
+} else {
+  console.log("Its warm outside");
+}
+if (!sunny) {
+  console.log("Its cloudy outside");
+} else {
+  console.log("Its sunny outside");
+}
