45735865746d168f2d239d9ee14c3d2844705bad
Vytautas <vzvytautasz@gmail.com>
2024-11-01 13:20:01 +0200

    Array

---
+++ 31. Array/index.js
@@ -0,0 +1,13 @@
+// array = think of it as a variable that can store multiple values.
+
+let fruits = ["apple", "orange", "banana"];
+// fruits[0] = "coconut"; // will update and change elements in array.
+fruits.push("lemon"); // will add an element
+fruits.pop(); // removes last element
+fruits.unshift("mango"); // add element to beginning
+fruits.shift(); // will remove element at beginning
+
+let length = fruits.length; // show lenght of array.
+let index = fruits.indexOf("orange"); // shows index of element in array
+
+console.log(index);
