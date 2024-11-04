2090930fd7e70641c0485380d6b5570dda0579a2
Vytautas <vzvytautasz@gmail.com>
2024-11-04 10:49:58 +0200

    Sort an array of strings.

---
+++ 33. Sort an array of strings/index.js
@@ -0,0 +1,8 @@
+let fruits = ["banana", "apple", "orange", "mango"];
+
+// fruits = fruits.sort(); // sort in alphabetical order.
+fruits = fruits.sort().reverse(); // sort in reverse alphabetical order.
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
