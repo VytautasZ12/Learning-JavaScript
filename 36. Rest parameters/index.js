11461dcecc343594746bf7decb0be28c4ac75df0
Vytautas <vzvytautasz@gmail.com>
2024-11-04 12:50:54 +0200

    Rest parameters.

---
+++ 36. Rest parameters/index.js
@@ -0,0 +1,36 @@
+// rest parameters = represents an indefinite number
+// ...               of parameters (packs arguments into an array)
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+// better way to do same as code below:
+console.log(sum(a, b, c, d));
+function sum(...numbers) {
+  let total = 0;
+  for (let number of numbers) {
+    total += number;
+  }
+  return total;
+}
+
+// console.log(sum(a, b));
+
+// function sum(a, b) {
+//   return a + b;
+// }
+
+// console.log(sum(a, b, c));
+
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+
+// console.log(sum(a, b, c, d));
+
+// function sum(a, b, c, d) {
+//   return a + b + c + d;
+// }
