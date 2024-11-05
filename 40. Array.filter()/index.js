f834a2ad743489c1c95020e52d23df2cf6a124d1
Vytautas <vzvytautasz@gmail.com>
2024-11-05 08:38:26 +0200

    Array.filter() method.

---
+++ 40. Array.filter()/index.js
@@ -0,0 +1,13 @@
+// array.filter() = creates a new array with all elements
+//                  that pass the test provided by a function.
+
+let ages = [18, 16, 21, 17, 19, 20];
+let adults = ages.filter(checkAge);
+adults.forEach(print);
+
+function checkAge(element) {
+  return element >= 18;
+}
+function print(element) {
+  console.log(element);
+}
